2f97ab8c97e89b25aa2f100037bf884c4f24ee5b
Stephen Oghenefego Okeh <stevlite29@gmail.com>
2025-01-21 22:20:01 +0100

    Version 1: Mobile view...

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Playfair: "'Playfair Display', serif",
+        Playwrite: "'Playwrite India', serif"
+      }
+    },
+  },
+  plugins: [],
+}
+
